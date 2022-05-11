7488511daaca0f9c45005e42fa826d82955c0846
xi-studio <110littletree@sina.com>
2022-05-11 17:48:25 +0800

    add 3d

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 **python main.py --base configs/radar_transformer.yaml -t True --gpus 3,
-**python main.py --base configs/radar_vqgan.yaml -t True --gpus 3,
+**python main.py --base configs/radar_transformer.yaml -t True --gpus 3,
+**python main.py --base configs/3d_vqgan.yaml -t True --gpus 3,
 
---
+++ taming/data/radar3d.py
@@ -0,0 +1,89 @@
+import os
+import numpy as np
+import albumentations
+import bisect
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class ImagePaths(Dataset):
+    def __init__(self, paths, size=None, random_crop=False, labels=None):
+        self.size = size
+        self.random_crop = random_crop
+
+        self.labels = dict() if labels is None else labels
+        self.labels["file_path_"] = paths
+        self._length = len(paths)
+
+        if self.size is not None and self.size > 0:
+            self.rescaler = albumentations.SmallestMaxSize(max_size = self.size)
+            if not self.random_crop:
+                self.cropper = albumentations.CenterCrop(height=self.size,width=self.size)
+            else:
+                self.cropper = albumentations.RandomCrop(height=self.size,width=self.size)
+            self.preprocessor = albumentations.Compose([self.rescaler, self.cropper])
+        else:
+            self.preprocessor = lambda **kwargs: kwargs
+
+    def __len__(self):
+        return self._length
+
+    def preprocess_image(self, image_path):
+        image = Image.open(image_path)
+        image = np.array(image).astype(np.uint8)
+        image = image.reshape((3, 256, 256))
+        image = np.transpose(image, (1,2,0))
+        image = self.preprocessor(image=image)["image"]
+        image = (image/127.5 - 1.0).astype(np.float32)
+        return image
+
+    def __getitem__(self, i):
+        example = dict()
+        example["image"] = self.preprocess_image(self.labels["file_path_"][i])
+        for k in self.labels:
+            example[k] = self.labels[k][i]
+        return example
+
+
+class FacesBase(Dataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.data = None
+        self.keys = None
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        example = self.data[i]
+        ex = {}
+        if self.keys is not None:
+            for k in self.keys:
+                ex[k] = example[k]
+        else:
+            ex = example
+        return ex
+
+
+class RadarHQTrain(FacesBase):
+    def __init__(self, size, crop_size=None, keys=None):
+        super().__init__()
+        root = "data/"
+        with open("data/train3d.txt", "r") as f:
+            relpaths = f.read().splitlines()
+        paths = [os.path.join(root, relpath) for relpath in relpaths]
+        self.data = ImagePaths(paths=paths, size=size, random_crop=False)
+        self.keys = keys
+
+
+class RadarHQValidation(FacesBase):
+    def __init__(self, size, crop_size=None, keys=None):
+        super().__init__()
+        root = "data/"
+        with open("data/validation3d.txt", "r") as f:
+            relpaths = f.read().splitlines()
+        paths = [os.path.join(root, relpath) for relpath in relpaths]
+        self.data = ImagePaths(paths=paths, size=size, random_crop=False)
+        self.keys = keys
+
